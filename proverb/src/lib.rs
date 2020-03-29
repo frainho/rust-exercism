825adb9786e86f5bb715c02f17c12b8fd89dfeff
Filipe Rainho <filipenrainho@gmail.com>
2020-03-29 21:09:50 +0200

    Add proverb solution

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,24 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    let mut words = list.iter().peekable();
+    let mut proverb = String::new();
+
+    while let Some(word) = words.next() {
+        let next_word = match words.peek() {
+            Some(next_word) => next_word,
+            None => {
+                proverb.push_str(&format!(
+                    "And all for the want of a {}.",
+                    list.get(0).unwrap()
+                ));
+                break;
+            }
+        };
+
+        proverb.push_str(&format!(
+            "For want of a {} the {} was lost.\n",
+            word, next_word
+        ));
+    }
+
+    proverb
 }
