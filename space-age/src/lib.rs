b73488ebeac1b422b9acfa6949b41ab73d195681
Filipe Rainho <filipenrainho@gmail.com>
2020-05-05 18:57:54 +0200

    update space age, final version

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -1,4 +1,4 @@
-const ONE_EARTH_YEAR_IN_SECONDS: f64 = 31557600.0;
+const ONE_EARTH_YEAR_IN_SECONDS: f64 = 31_557_600.0;
 
 #[derive(Debug)]
 pub struct Duration(f64);
@@ -12,7 +12,7 @@ impl From<u64> for Duration {
 pub trait Planet {
     const ONE_YEAR_FACTOR_EARTH: f64;
     fn years_during(d: &Duration) -> f64 {
-        Earth::years_during(d) / Self::ONE_YEAR_FACTOR_EARTH
+        (d.0 / ONE_EARTH_YEAR_IN_SECONDS) / Self::ONE_YEAR_FACTOR_EARTH
     }
 }
 
@@ -26,29 +26,26 @@ pub struct Uranus;
 pub struct Neptune;
 
 impl Planet for Mercury {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 0.2408467;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 0.240_846_7;
 }
 impl Planet for Venus {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 0.61519726;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 0.615_197_26;
 }
 impl Planet for Earth {
     const ONE_YEAR_FACTOR_EARTH: f64 = 1.0;
-    fn years_during(d: &Duration) -> f64 {
-        d.0 / ONE_EARTH_YEAR_IN_SECONDS
-    }
 }
 impl Planet for Mars {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 1.8808158;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 1.880_815_8;
 }
 impl Planet for Jupiter {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 11.862615;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 11.862_615;
 }
 impl Planet for Saturn {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 29.447498;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 29.447_498;
 }
 impl Planet for Uranus {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 84.016846;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 84.016_846;
 }
 impl Planet for Neptune {
-    const ONE_YEAR_FACTOR_EARTH: f64 = 164.79132;
+    const ONE_YEAR_FACTOR_EARTH: f64 = 164.791_32;
 }
